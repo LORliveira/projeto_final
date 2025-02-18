8386b79ce87a466307b6bdbd592a332937ca3c59
Bryan70897 <bryansmith.cavalcante@gmail.com>
2025-02-18 15:11:05 -0300

    Coloquei as rotas e criei um DataRecord

--- backend/aplicação/controladores/rotas.py
+++ backend/aplicação/controladores/rotas.py
@@ -1,11 +1,49 @@
 
-from flask import Flask
+from flask import render_template, redirect, request, session, url_for
+from app import app
+from controladores.DataRecord import DataRecord
 
-app = Flask(__name__)
+ctl = DataRecord() # Não esquecer de fazer o data record que o controlador de dados
 
-@app.route('/')
+@app.route('index')
 def serve_frontend():
     return ('../frontend/build', 'index.html')
+
+@app.route('/')
+def index():
+    return render_template('helper.html')
+
+@app.route('/pagina', methods=['GET'])
+@app.route('/pagina/<username>', methods=['GET'])
+def action_pagina(username=None):
+    if not username:
+        return render_template('pagina.html', transfered=False)
+    else:
+        info = ctl.work_with_parameter(username)
+        if not info:
+             return redirect(url_for('index'))
+        else:
+            return render_template('pagina.html', transfered=True, data=info)
+
+@app.route('/portal', methods=['GET', 'POST'])
+def portal():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        session_id, username = ctl.authenticate_user(username, password)
+        if session_id:
+            session['session_id'] = session_id
+            return render_template(url_for('portal'))
+        else:
+            return redirect(url_for('portal'))
+    return render_template('portal.html')
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    ctl.logout_user()
+    session.pop('session_id', None)
+    return redirect(url_for('index'))
+            
 
 @app.route('/api')
 def api_endpoint():
